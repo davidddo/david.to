38ec0a50f7a4feac07fd9573fe66b5203934f59e
David Tomschitz <david@tomschitz.de>
2021-07-08 23:40:59 +0200

    feat: added icons for the social buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
     AppRoutingModule,
     MaterialModule,
     CoreModule,
--- src/app/core/containers/app-root/app.component.ts
+++ src/app/core/containers/app-root/app.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+const icons = [
+  {
+    tag: 'github',
+    file: 'github.svg',
+  },
+  {
+    tag: 'linkedin',
+    file: 'linkedin.svg',
+  },
+  {
+    tag: 'xing',
+    file: 'xing.svg',
+  },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    for (const icon of icons) {
+      this.matIconRegistry.addSvgIcon(
+        icon.tag,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          `../assets/icons/${icon.file}`,
+        ),
+      );
+    }
+  }
+}
