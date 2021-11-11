d1811dd1207219933c575fcc0d40e5ca566f7cd6
David Tomschitz <david@tomschitz.de>
2021-11-11 11:59:12 +0100

    feat: add the option to clear the specific collection

--- tools/seed.js
+++ tools/seed.js
@@ -4,7 +4,10 @@ const { initializeApp } = require('firebase/app');
 const {
   getFirestore,
   writeBatch,
+  query,
   addDoc,
+  deleteDoc,
+  getDocs,
   collection,
 } = require('firebase/firestore');
 const config = require('./config');
@@ -13,11 +16,23 @@ async function seed() {
   initializeApp(config);
 
   const database = getFirestore();
+  const collectionRef = collection(database, argv.collection);
+
+  if (argv.clear) {
+    const docs = await getDocs(query(collectionRef));
+    const batch = writeBatch(database);
+
+    docs.forEach(doc => batch.delete(doc.ref));
+    await batch.commit();
+    console.log('🗑️   Collection cleared');
+  }
 
   const data = require(argv.data);
-  data.forEach(async object => {
-    await addDoc(collection(database, argv.collection), object);
-  });
+  const tasks = data.map(object => addDoc(collectionRef, object));
+  await Promise.all(tasks);
+
+  console.log('🚀  Data uploaded');
+  process.exit(0);
 }
 
 seed();
