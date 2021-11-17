946321579f17068a2d0d305e0a917a1e43d0baa0
David Tomschitz <david@tomschitz.de>
2021-11-17 14:25:07 +0100

    fix: update the firestore converter in order to parse timestamps

--- src/app/store/educations/educations.service.ts
+++ src/app/store/educations/educations.service.ts
@@ -2,16 +2,28 @@ import { Injectable } from '@angular/core';
 import { collectionData, Firestore } from '@angular/fire/firestore';
 import { collection, CollectionReference } from '@firebase/firestore';
 import { Education } from '@website/models';
-import { firestoreConverter } from '@website/store/utils';
+import { serializeDateRange } from '@website/store/utils';
 
 @Injectable({ providedIn: 'root' })
 export class EducationsService {
   private collectionRef: CollectionReference<Education>;
 
   constructor(firestore: Firestore) {
-    this.collectionRef = collection(firestore, 'educations').withConverter(
-      firestoreConverter<Education>(),
-    );
+    this.collectionRef = collection(
+      firestore,
+      'educations',
+    ).withConverter<Education>({
+      toFirestore: (data: Education) => data,
+      fromFirestore: (snapsot, options) => {
+        const data = snapsot.data();
+
+        return {
+          ...data,
+          id: snapsot.id,
+          dateRange: serializeDateRange(data.dateRange),
+        } as Education;
+      },
+    });
   }
 
   fetchEducations() {
--- src/app/store/experiences/experiences.service.ts
+++ src/app/store/experiences/experiences.service.ts
@@ -2,16 +2,32 @@ import { Injectable } from '@angular/core';
 import { collectionData, Firestore } from '@angular/fire/firestore';
 import { collection, CollectionReference } from '@firebase/firestore';
 import { WorkExperience } from '@website/models';
-import { firestoreConverter } from '@website/store/utils';
+import { serializeDateRange } from '@website/store/utils';
 
 @Injectable({ providedIn: 'root' })
 export class ExperiencesService {
   private collectionRef: CollectionReference<WorkExperience>;
 
   constructor(firestore: Firestore) {
-    this.collectionRef = collection(firestore, 'experiences').withConverter(
-      firestoreConverter<WorkExperience>(),
-    );
+    this.collectionRef = collection(
+      firestore,
+      'experiences',
+    ).withConverter<WorkExperience>({
+      toFirestore: (data: WorkExperience) => data,
+      fromFirestore: (snapsot, options) => {
+        const data = snapsot.data();
+
+        return {
+          ...data,
+          id: snapsot.id,
+          dateRange: serializeDateRange(data.dateRange),
+          sections: data.sections?.map(section => ({
+            ...section,
+            dateRange: serializeDateRange(section.dateRange),
+          })),
+        } as WorkExperience;
+      },
+    });
   }
 
   fetchWorkExperiences() {
