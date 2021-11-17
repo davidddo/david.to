aeba796de644fc1263f8f4b4d5f38dfa1650e900
David Tomschitz <david@tomschitz.de>
2021-11-17 14:28:26 +0100

    fix: remove the app bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  HostListener,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 import { Education, Project, WorkExperience } from '@website/models';
@@ -19,41 +14,12 @@ import * as fromEducations from '@website/store/educations';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('fade', [
-      transition(':enter', [
-        style({ height: '0px', opacity: 0 }),
-        animate(
-          '280ms cubic-bezier(0.4, 0, 0.2, 1)',
-          style({ height: '64px', opacity: 1 }),
-        ),
-      ]),
-      transition(':leave', [
-        style({ height: '64px', opacity: 1 }),
-        animate(
-          '280ms cubic-bezier(0.4, 0, 0.2, 1)',
-          style({ height: '0px', opacity: 0 }),
-        ),
-      ]),
-    ]),
-  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   projects$: Observable<Project[]>;
   experiences$: Observable<WorkExperience[]>;
   educations$: Observable<Education[]>;
-
-  showHeader: boolean;
-
-  @HostListener('window:scroll')
-  onWindowScroll() {
-    if (window.scrollY >= 156) {
-      this.showHeader = true;
-    } else {
-      this.showHeader = false;
-    }
-  }
 
   constructor(private store: Store<fromEducations.State>) {
     this.projects$ = this.store.pipe(select(fromProjects.selectAll));
