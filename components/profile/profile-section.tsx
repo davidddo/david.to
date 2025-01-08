e6b2a298025139e43a0329d091fb43be9003e3cd
dtomschitz <david@tomschitz.de>
2025-01-08 15:22:40 +0100

    remove unused import

--- components/profile/profile-section.tsx
+++ components/profile/profile-section.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import Card, {CardContent} from '@/components/ui/card';
 import Button from '@/components/ui/button';
 import AnimatedLink from '@/components/ui/animated-link';
